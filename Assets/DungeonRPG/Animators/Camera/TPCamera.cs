169d5fd2c1f5a7d1e9a0ccbc6f62346b58b7eb34
furu8 <foooolurk1125@gmail.com>
2020-03-13 21:22:54 +0900

    コメントアウト、初期値更新

--- Assets/DungeonRPG/Animators/Camera/TPCamera.cs
+++ Assets/DungeonRPG/Animators/Camera/TPCamera.cs
@@ -3,69 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 [ExecuteInEditMode, DisallowMultipleComponent]
 public class TPCamera : MonoBehaviour {
-    
-    // private GameObject player;       //プレイヤー格納用
-    // [SerializeField]
-    // private float rotateSpeed = 3f; //回転させるスピード
-
-
-	// // Use this for initialization
-	// void Start () {
-    //     //unitychanをplayerに格納
-    //     player = GameObject.Find("Player");
-	// }
-	
-	// // Update is called once per frame
-    // void Update () {
-    //     //左シフトが押されている時
-    //     if (Input.GetKey(KeyCode.LeftShift))
-    //     {
-    //         //ユニティちゃんを中心に-5f度回転
-    //         transform.RotateAround(player.transform.position, Vector3.up, -rotateSpeed);
-    //     }
-    //     //右シフトが押されている時
-    //     else if(Input.GetKey(KeyCode.RightShift))
-    //     {
-    //         //ユニティちゃんを中心に5f度回転
-    //         transform.RotateAround(player.transform.position, Vector3.up, rotateSpeed);
-    //     }
-    // private GameObject mainCamera;              //メインカメラ格納用
-    // private GameObject playerObject;            //回転の中心となるプレイヤー格納用
-    // public float rotateSpeed = 2.0f;            //回転の速さ
- 
-    // //呼び出し時に実行される関数
-    // void Start()
-    // {
-    //     //メインカメラとユニティちゃんをそれぞれ取得
-    //     mainCamera = Camera.main.gameObject;
-    //     playerObject = GameObject.Find("Player");
-    // }
- 
- 
-    // //単位時間ごとに実行される関数
-    // void Update()
-    // {
-    //     //rotateCameraの呼び出し
-    //     rotateCamera();
-    // }
- 
-    // //カメラを回転させる関数
-    // private void rotateCamera()
-    // {
-    //     //Vector3でX,Y方向の回転の度合いを定義
-    //     Vector3 angle = new Vector3(Input.GetAxis("Mouse X") * rotateSpeed,Input.GetAxis("Mouse Y") * rotateSpeed, 0);
- 
-    //     //transform.RotateAround()をしようしてメインカメラを回転させる
-    //     mainCamera.transform.RotateAround(playerObject.transform.position, Vector3.up, angle.x);
-    //     mainCamera.transform.RotateAround(playerObject.transform.position, transform.right, angle.y);
-    // }
 
     public GameObject target; // an object to follow
     public Vector3 offset; // offset form the target object
 
     [SerializeField] private float distance = 4.0f; // distance from following object
-    [SerializeField] private float polarAngle = 45.0f; // angle with y-axis
-    [SerializeField] private float azimuthalAngle = 45.0f; // angle with x-axis
+    [SerializeField] private float polarAngle = 0f; // angle with y-axis
+    [SerializeField] private float azimuthalAngle = 0f; // angle with x-axis
 
     [SerializeField] private float minDistance = 1.0f; // ホイールで近づける最小距離
     [SerializeField] private float maxDistance = 7.0f; // ホイールで遠ざける最大距離
@@ -80,6 +24,7 @@ public class TPCamera : MonoBehaviour {
         target = GameObject.Find("Player");
 	}
 
+    // カメラはPlayerよりもあとに必ずUpdateするためにLateUpdate
     void LateUpdate()
     {
         // マウスの右クリック押したら
