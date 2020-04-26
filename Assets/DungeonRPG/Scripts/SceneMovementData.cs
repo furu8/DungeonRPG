b46b47464bcf2ba20ef338ac4c972bd64c923785
furu8 <foooolurk1125@gmail.com>
2020-04-26 16:50:01 +0900

    ダンジョンシーン作成

---
+++ Assets/DungeonRPG/Scripts/SceneMovementData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+[Serializable]
+[CreateAssetMenu(fileName = "SceneMovementData", menuName = "CreateSceneMovementData")]
+
+public class SceneMovementData : ScriptableObject
+{
+    // シーンの遷移の列挙体
+    public enum SceneType {
+        StartGame,
+        FirstVillage,
+        FirstVillageToWorldMap
+    }
+    [SerializeField]
+    private SceneType sceneType;
+
+    // sceneTypeの初期化 
+    public void OnEnable() {
+        sceneType = SceneType.StartGame;
+    }
+    
+    // シーンタイプを設定
+    public void SetSceneType(SceneType scene) {
+        sceneType = scene;
+    }
+ 
+    // シーンタイプを返却
+    public SceneType GetSceneType() {
+        return sceneType;
+    }
+}
+ 
