28c886c2ca50c7588503e513963c0effd940c51f
furu8 <foooolurk1125@gmail.com>
2020-06-09 02:54:58 +0900

    モンスターデータ作成

---
+++ Assets/DungeonRPG/Scripts/Status/MonsterPartyStatus.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+[Serializable]
+[CreateAssetMenu(fileName = "MonsterPartyStatus", menuName = "CreateMonsterPartyStatus")]
+public class MonsterPartyStatus : ScriptableObject
+{
+    [SerializeField]
+    private string partyName = null;
+    [SerializeField]
+    private List<GameObject> partyMembers = null;
+ 
+    public string GetPartyName() {
+        return partyName;
+    }
+ 
+    public List<GameObject> GetMonsterGameObjectList() {
+        return partyMembers;
+    }
+}
---
+++ Assets/DungeonRPG/Scripts/Status/MonsterStatus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+[Serializable]
+[CreateAssetMenu(fileName = "MonsterStatus", menuName = "CreateMonsterStatus")]
+public class MonsterStatus : CharacterStatus
+{
+ 
+}
