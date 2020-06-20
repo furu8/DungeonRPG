43288a8a288d4547577316a345909b4baddbffa6
furu8 <foooolurk1125@gmail.com>
2020-06-20 16:06:04 +0900

    BattleField

---
+++ Assets/DungeonRPG/Scripts/Status/MonsterPartyStatusList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+[Serializable]
+[CreateAssetMenu(fileName = "MonsterPartyStatusList", menuName = "CreateMonsterPartyStatusList")]
+public class EnemyPartyStatusList : ScriptableObject
+{
+    [SerializeField]
+    private List<MonsterPartyStatus> partyMembersList = null;
+ 
+    public List<MonsterPartyStatus> GetPartyMembersList() {
+        return partyMembersList;
+    }
+}
+
